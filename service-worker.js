b628ba7c164af2c610680bf9bd5f1ac96404f3e9
DreadedSol <lostsolwil@gmail.com>
2025-05-21 21:31:46 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 // service-worker.js
 const CACHE_NAME = 'cursed-treasures-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './service-worker.js',
-  './images/icon-192.png',
-  './images/icon-512.png',
-  './images/parchment-map.png',
-  // add every other file you reference
+  './',                   // the root of your app
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'images/icon-192.png',
+  'images/icon-512.png',
+  'images/parchment-map.png',
+  'images/parchment-player2.png',
+  'images/parchment-tile.png',
+  // …and any other files (CSS, JS, fonts, more images) exactly as they appear in your repo
 ];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
-      .catch(err => {
-        console.error('Cache addAll failed:', err);
-      })
+      .catch(err => console.error('Cache addAll failed:', err))
       .then(() => self.skipWaiting())
   );
 });
