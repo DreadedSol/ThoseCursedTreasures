f6bcae8bb42561e7c86c3c598bd7097faa58dc35
DreadedSol <lostsolwil@gmail.com>
2025-05-21 21:30:14 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,40 @@
+// service-worker.js
 const CACHE_NAME = 'cursed-treasures-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  '/images/icon-192.png',
-  '/images/icon-512.png',
-  // ...all your CSS, JS, images, fonts, etc.
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './images/icon-192.png',
+  './images/icon-512.png',
+  './images/parchment-map.png',
+  // add every other file you reference
 ];
 
-// 1) Install: cache all the assets
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
+      .catch(err => {
+        console.error('Cache addAll failed:', err);
+      })
       .then(() => self.skipWaiting())
   );
 });
 
-// 2) Activate: clean up old caches
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+          .map(oldKey => caches.delete(oldKey))
       )
     )
   );
   self.clients.claim();
 });
 
-// 3) Fetch: serve from cache, fall back to network
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request)
